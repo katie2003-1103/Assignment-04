4a32c8fd1a87d3e22b6c6236ed9b42a928eeefd5
Kaitlyn Gonzalez <kaitlyn.l.gonzalez@gmail.com>
2025-10-22 16:59:40 -0700

    Initial Commit + function practice

---
+++ js/01-functions-practice.js
@@ -0,0 +1,23 @@
+//STEP 1
+function halfNumber(num){
+    const results = num / 2
+    console.log(`Half of ${num} is ${results}`) 
+    return results
+}
+//STEP 2
+function squareNumber(num){
+    const results = num * num 
+    console.log(`The result of squaring the number ${num} is ${results}`)
+    return results
+}
+//STEP 3
+function percentOf(num1, num2) {
+    const results = num1 / num2 * 100 
+    console.log(`${num1} is ${results} % of ${num2}`)
+    return results
+}
+//STEP 4
+function findModulus(num1, num2){
+    const results = num1 % num2 
+    console.log(`${results} is the modulus of ${num1} and ${num2}`)
+} 
